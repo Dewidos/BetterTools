baea8f3d955d283e1ee0413ebbbb2ab1d42c54bb
Dewidos <minecraftdewidos@gmail.com>
2022-02-26 15:04:49 +0100

    Improved Creeperium Shovel's special ability

--- src/main/java/me/dewidos/bettertools/BetterTools.java
+++ src/main/java/me/dewidos/bettertools/BetterTools.java
@@ -2,11 +2,13 @@ package me.dewidos.bettertools;
 
 import me.dewidos.bettertools.blocks.ModBlocks;
 import me.dewidos.bettertools.items.ModItems;
+import me.dewidos.bettertools.items.tools.CreeperiumShovel;
 import me.dewidos.bettertools.items.tools.CreeperiumSword;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
@@ -14,7 +16,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -44,6 +45,7 @@ public class BetterTools
         eventBus.addListener(this::processIMC);
 
         MinecraftForge.EVENT_BUS.register(CreeperiumSword.class);
+        MinecraftForge.EVENT_BUS.register(CreeperiumShovel.class);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/me/dewidos/bettertools/items/tools/CreeperiumShovel.java
+++ src/main/java/me/dewidos/bettertools/items/tools/CreeperiumShovel.java
@@ -2,21 +2,21 @@ package me.dewidos.bettertools.items.tools;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShovelItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Date;
 
 public class CreeperiumShovel extends ShovelItem {
-    private MobEffectInstance hasteEffect = null;
+    private Date abilityEndDate = new Date();
 
     public CreeperiumShovel(Tier pTier, float pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
@@ -26,29 +26,24 @@ public class CreeperiumShovel extends ShovelItem {
     @ParametersAreNonnullByDefault
     public boolean mineBlock(ItemStack pStack, Level pLevel, BlockState pState, BlockPos pPos, LivingEntity pEntityLiving) {
         if (pLevel.random.nextFloat() < 0.1f) {
-            pEntityLiving.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, 100, 3, false, false, false));
-            hasteEffect = pEntityLiving.getEffect(MobEffects.DIG_SPEED);
-
+            abilityEndDate = new Date(System.currentTimeMillis() + 5000);
             pLevel.playSound(null, pEntityLiving.getX(), pEntityLiving.getY(), pEntityLiving.getZ(), SoundEvents.GENERIC_EXPLODE, pEntityLiving.getSoundSource(), 2.0F, (1.0F + (pLevel.random.nextFloat() - pLevel.random.nextFloat()) * 0.2F) * 0.7F);
         }
 
         return super.mineBlock(pStack, pLevel, pState, pPos, pEntityLiving);
     }
 
-    @Override
-    @ParametersAreNonnullByDefault
-    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
-        if (hasteEffect == null) return;
+    @SubscribeEvent
+    public static void creeperiumShovelAbility(PlayerEvent.BreakSpeed event) {
+        LivingEntity livingEntity = event.getEntityLiving();
+        if (livingEntity == null) return;
 
-        if (!pIsSelected && !pLevel.isClientSide) {
-            if (pEntity instanceof LivingEntity livingEntity) {
-                if (livingEntity.getEffect(MobEffects.DIG_SPEED) == hasteEffect) {
-                    livingEntity.removeEffect(MobEffects.DIG_SPEED);
-                }
-                hasteEffect = null;
+        Item item = livingEntity.getMainHandItem().getItem();
+
+        if (item instanceof CreeperiumShovel creeperiumShovel) {
+            if (new Date().before(creeperiumShovel.abilityEndDate)) {
+                event.setNewSpeed(event.getOriginalSpeed() * 3f);
             }
         }
-
-        super.inventoryTick(pStack, pLevel, pEntity, pSlotId, pIsSelected);
     }
 }
